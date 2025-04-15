f1fdb069b593a9ced77d1b7f239ba61b9d62f55a
ROHITH SAGAR M <rohithsagar14325@gmail.com>
2025-04-15 23:40:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,10 +12,7 @@ export default defineConfig({
       },
     },
   },
-    resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    
   },
   build: {
     outDir: "dist",
